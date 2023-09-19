e82efccdd2fb5a90d4f940b95e80d41f01bb09b3
Sajal Chauhan <sajalchauhan2004@gmail.com>
2023-09-19 13:57:23 +0530

    Updated Classes, Added Spaceship

--- myGame/Js/script.js
+++ myGame/Js/script.js
@@ -1,13 +1,147 @@
 // Plan -> Classes for Asteroids, Bullets => Bullets come from SpaceShip
 
+class Asteroids{
+    constructor(height, width, side){
+        // const obstacleImage = new Image()
+        // obstacleImage.src = null // REPLACE THIS WITH IMAGE PATH
+        this.height = height
+        this.width = width
+        this.side = side
+        this.asteroids = []
+        this.speed = OBSTACLE_SPEED
+    }
+
+    generate() {
+        rand_x = Math.floor(Math.random() * (canvas.width)) + 10
+        this.asteroids.push([
+            this.height,
+            this.side,
+            this.rand_x,
+            false 
+        ])
+    }
+
+    draw(){
+        for(const i of this.asteroids){
+            if(i[0] <= 0 ){
+                continue;
+            }
+            ctx.fillStyle = '#0000FF'
+            ctx.fillRect(this.width, this.height, this.side, this.side)
+        }
+    }
+
+    update(){
+        for(const i of this.asteroids){
+            // update asteroid
+            i[0] -= this.speed
+            if (i[0] <= 0){
+                const index = this.asteroids.indexOf(i);
+                if (index !== -1) {
+                this.asteroids.splice(index, 1);
+                }
+            }
+        }
+    }
+
+    // if bullet hits the asteroid
+    checkDestroyed(){
+        for(const i of this.asteroids){
+            if(i[0] <= 0){
+                return true
+            }
+        }
+    }
+}
+
+class Bullet{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+        this.speed = BULLET_SPEED
+        this.width = 5
+        this.height = 10
+        this.color = '#FF0000'
+    }
+
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update(){
+        this.y -= this.speed
+    }
+
+    isOutOfCanvas(){
+        if(this.y <= 0){
+            return true
+        }
+    }
+}
+
+class Spaceship {
+    constructor() {
+        this.x = canvas.width / 2
+        this.y = 50
+        this.width = 50 
+        this.height = 30 
+        this.speed = PLAYER_SPEED 
+        this.color = "#00FF00"
+        this.isMovingLeft = false 
+        this.isMovingRight = false 
+    }
+
+    moveLeft() {
+        this.isMovingLeft = true;
+        this.isMovingRight = false;
+    }
+
+    moveRight() {
+        this.isMovingLeft = false;
+        this.isMovingRight = true;
+    }
+
+    stop() {
+        this.isMovingLeft = false;
+        this.isMovingRight = false;
+    }
+
+    // Handle User Input
+    handleInput() {
+        if (this.isMovingLeft) {
+            this.x -= this.speed;
+        } else if (this.isMovingRight) {
+            this.x += this.speed;
+        }
+
+        if (this.x < 0) {
+            this.x = 0;
+        } else if (this.x + this.width > canvas.width) {
+            this.x = canvas.width - this.width;
+        }
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    shoot() {
+        const bulletX = this.x + this.width / 2;
+        const bulletY = this.y;
+        return new Bullet(bulletX, bulletY);
+    }
+}
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const BG_COLOR = '#121212';
-const PLAYER_SPEED = 1;
+const BG_COLOR = '#121212'
+const PLAYER_SPEED = 2;
 const BULLET_SPEED = 10;
 const OBSTACLE_SPEED = 5
 
@@ -22,79 +156,7 @@ const background = (color) => {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-class Obstacles{
-    constructor(height, width, side){
-        const obstacleImage = new Image()
-        obstacleImage.src = null // REPLACE THIS WITH IMAGE PATH
-        this.height = height
-        this.width = width
-        this.side = side
-        this.obstacles = []
-        this.speed = 20
-        this.genTime = 1.2
-        this.genTimeScore = 2000
-        this.score = 0
-    }
+function gameloop(){
 
-    // create obstacle function
-    generate(this) {
-        randomYCoordinate = Math.floor(Math.random() * (canvas.width)) + 10;
-        obstacles.push([
-            this.width,
-            this.side,
-            this.randomYCoordinate,
-            false
-        ])
-    }
-
-    // render obstacle on canvas
-    draw(this){
-        for(let obstacle=0; obstacle <= obstacles; obstacle++){
-            if(obstacle[0] <=0 ){
-                continue;
-            }
-            // WRITE THE DRAWING CODE
-            
-        }
-    }
-
-    // move the asteroid from right to left
-    update(this){
-        for(let obstacle=0; obstacle <= obstacles; obstacle++){
-            obstacle[0] -= this.speed
-            if (obstacle[0] <= 0){
-                const index = obstacles.indexOf(obstacle);
-                if (index !== -1) {
-                obstacles.splice(index, 1);
-                }
-            }
-        }
-    }
-
-    // if bullet hits the asteroid
-    checkDestroyed(this){
-
-    }
 }
 
-class Bullet{
-    constructor(){
-        
-    }
-
-    generate(){
-
-    }
-
-    draw(){
-
-    }
-
-    update(){
-
-    }
-
-    checkDestroyed(){
-
-    }
-}
